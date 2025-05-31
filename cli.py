6f924409796b7f1815280384c876818f0dfcbd1a
Pavel Scherbakov <pyuscherbakov@gmail.com>
2025-06-01 01:43:43 +0300

    CLI

--- cli.py
+++ cli.py
@@ -1,38 +1,69 @@
 import cmd
+from controller import PhoneBookController
 
 
 class PhoneBookCLI(cmd.Cmd):
+    intro = "📒Записная книжка. Введите help или ? для справки."
     prompt = "> "
+    commands = {
+        "1": "Добавить контакт",
+        "2": "Удалить контакт",
+        "3": "Получить контакт",
+        "4": "Получить все контакты",
+        "5": "Поиск контактов",
+        "6": "Обновить контакт",
+        "exit": "Выход"
+    }
+    
+    def __init__(self):
+        super().__init__()
+        self._controller = PhoneBookController()
 
-    def preloop(self):
-        print("📒 Записная книжка. Введите help или ? для справки.")
-        print("commands list") # TODO: Определить список команд
+    def do_1(self, arg):
+        """Добавить новый контакт"""
+        name = input("Введите имя контакта: ")
+        phone = input("Введите номер телефона: ")
+        comment = input("Введите комментарий: ")
+        self._controller.add_contact(name, phone, comment)
 
-    def do_add_contact(self, args):
-        """Adds a new contact."""
-        pass
+    def do_2(self, arg):
+        """Удалить контакт"""
+        contact_id = int(input("Введите ID контакта: "))
+        self._controller.delete_contact(contact_id)
 
-    def do_remove_contact(self, args):
-        """Removes a contact."""
-        pass
+    def do_3(self, arg):
+        """Обновить контакт"""
+        contact_id = int(input("Введите ID контакта: "))
+        self._controller.get_contact(contact_id)
 
-    def do_get_contact(self, args):
-        """Gets a contact."""
-        pass
+    def do_4(self, arg):
+        """Получить все контакты"""
+        self._controller.show_all_contacts()
 
-    def do_get_all_contacts(self, args):
-        """Gets all contacts."""
+    def do_5(self, arg):
+        """Найти контакт/ы"""
+        query = input("Введите запрос: ")
+        self._controller.find_contacts(query)
 
-    def do_find_contact(self, args):
-        """Finds a contact."""
+    def do_6(self, arg):
+        """Обновить контакт"""
+        contact_id = int(input("Введите ID контакта: "))
+        name = input("Введите новое имя контакта: ")
+        phone = input("Введите новый номер телефона: ")
+        comment = input("Введите новый комментарий: ")
+        self._controller.update_contact(contact_id, name, phone, comment)
 
-    def do_update_contact(self, args):
-        """Updates a contact."""
-        pass
-
-    def do_exit(self, args):
-        """Exits the program."""
+    def do_exit(self, arg):  # NOQA
+        """Выйти из программы"""
         return True
+
+    def do_help(self, arg):
+        print("Список доступных команд:")
+        for command, description in self.commands.items():
+            print(f"{command}: {description}")
+
+    def default(self, line):
+        print(f"❌ Ошибка: команда '{line}' не распознана. Введите help или ? для справки.")
 
 
 if __name__ == '__main__':
